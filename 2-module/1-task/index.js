29da231e1c0379b4a4baf857ff1f508a70d2ac8d
tsareva.ria <tsareva.ria@gmail.com>
2023-08-26 17:15:22 +0300

    Module1 all tasks and 1 task of Module2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sumSalary = 0;
+
+  for (let key in salaries) {
+    if (!(isNaN(salaries[key])) &&
+      (isFinite(salaries[key])) &&
+      (typeof salaries[key] == "number")) {
+      sumSalary += +salaries[key];
+    }
+  }
+
+  return sumSalary;
 }
